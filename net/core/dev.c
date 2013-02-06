e8a68c2806cc71473137c4c775d911f3d5000ec6
Igor Maravic <igorm@etf.rs>
2013-02-06 11:00:34 +0100

    net: Added function for setting dev flags with short
    When net_device flags were set with unsigned short, upper 16 flag bits were ignored.
    So, when setting net_device flags with unsigned short, copy upper 16 flag bits to the
    change flag request.

--- net/core/dev.c
+++ net/core/dev.c
@@ -4857,6 +4857,15 @@ int dev_change_flags(struct net_device *dev, unsigned int flags)
 }
 EXPORT_SYMBOL(dev_change_flags);
 
+int dev_change_flags_short(struct net_device *dev, short s_flags)
+{
+	unsigned int flags = dev_get_flags(dev);
+	/* Combine left 16 bits with s_flags */
+	flags = (flags << sizeof(short) >> sizeof(short)) | s_flags;
+	return dev_change_flags(dev, flags);
+}
+EXPORT_SYMBOL(dev_change_flags_short);
+
 /**
  *	dev_set_mtu - Change maximum transfer unit
  *	@dev: device
@@ -5011,7 +5020,7 @@ static int dev_ifsioc(struct net *net, struct ifreq *ifr, unsigned int cmd)
 
 	switch (cmd) {
 	case SIOCSIFFLAGS:	/* Set interface flags */
-		return dev_change_flags(dev, ifr->ifr_flags);
+		return dev_change_flags_short(dev, ifr->ifr_flags);
 
 	case SIOCSIFMETRIC:	/* Set the metric on the interface
 				   (currently unused) */
